3554288e37de0dfc8ef305bccdcc3daaa929f73f
Yashvi Smart <yashvipsmart@gmail.com>
2026-02-15 17:56:30 +0530

    Included search functionality in the courses page so that it focuses on the input using useRef
    implemented useEffect and included search terms in the courses page
    using useReducer in the profile page
    implement form like attributes taking the name, email and proceeding to the next step using the useReducer technology

--- student-dashboard/src/pages/Courses.tsx
+++ student-dashboard/src/pages/Courses.tsx
@@ -1,3 +1,28 @@
-export default function Courses(){
-    return <><h1>Courses Page</h1></>
+import { useRef, useEffect, useState } from "react";
+export default function Courses() {
+  const [searchTerms, setSearchTerm]=useState('');
+  {
+    /*useRef accesses the input inside the DOM if 
+    there is none it stores null 
+    otherwise it stores the reference of the input
+    
+    it works after component is rendered*/
+  }
+  const searchRef = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    {
+      /*searchRef.current → DOM element or null
+        ?. → prevents crash if null
+        .focus() → browser API,
+        [] means the useEffect runs every time the componenet is remounted */
+    }
+    searchRef.current?.focus();
+  }, []);
+  return (
+    <>
+      <input ref={searchRef} value={searchTerms} onChange={(e)=>{setSearchTerm(e.target.value)}} type="text" placeholder="Search Couses..." />
+      <h1>Courses Page</h1>
+      <p>Searching For: {searchTerms}</p>
+    </>
+  );
 }
--- student-dashboard/src/pages/Profile.tsx
+++ student-dashboard/src/pages/Profile.tsx
@@ -1,7 +1,71 @@
-export default function Courses() {
+import { useReducer } from "react";
+type ProfileState = {
+  name: string;
+  email: string;
+  step: number;
+};
+
+type Action =
+  | { type: "SET_NAME"; payload: string } //union type having action as SET_NAME and payload(the value to be set) as string
+  | { type: "SET_EMAIL"; payload: string }
+  | { type: "NEXT_STEP" };
+
+export default function Profile() {
+  const initialState: ProfileState = {
+    name: "",
+    email: "",
+    step: 1,
+  };
+  const [state, dispatch] = useReducer(profileReducer, initialState);
+  function profileReducer(state: ProfileState, action: Action): ProfileState {
+    switch (action.type) {
+      case "SET_NAME":
+        return { ...state, name: action.payload };
+      case "SET_EMAIL":
+        return { ...state, email: action.payload };
+      case "NEXT_STEP":
+        return { ...state, step: state.step + 1 };
+
+      default:
+        return state;
+    }
+  }
   return (
     <>
-      <h1>Student Profile Page</h1>
+      <div>
+        <h2>Profile setup</h2>
+        {state.step == 1 && (
+          <div>
+            <input
+              type="text"
+              placeholder="Enter name"
+              value={state.name}
+              onChange={(e) => {
+                dispatch({ type: "SET_NAME", payload: e.target.value });
+              }}
+            />
+          </div>
+        )}
+        {state.step===1 && (  //like a confition
+        <button onClick={() => dispatch({ type: "NEXT_STEP" })}>
+          Next Step
+        </button>)}
+        {state.step == 2 && (
+          <div>
+            <input
+              type="text"
+              placeholder="Enter email"
+              value={state.email}
+              onChange={(e) => {
+                dispatch({ type: "SET_EMAIL", payload: e.target.value });
+              }}
+            />
+            <button onClick={() => dispatch({ type: "NEXT_STEP" })}>
+              Finish
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 }
